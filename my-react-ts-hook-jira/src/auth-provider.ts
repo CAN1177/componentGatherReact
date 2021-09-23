6ec154115b1dab77901434bf0d3125ffa5558c57
CAN7 <39960526+CAN1177@users.noreply.github.com>
2021-09-24 00:04:50 +0800

    feat: 模拟第三方auth服务

---
+++ my-react-ts-hook-jira/src/auth-provider.ts
@@ -0,0 +1,43 @@
+//  真实环境中一般使用 firebase 这种第三方auth 服务， 则不需要此文件的开发 
+import { User } from 'screens/project-list/search-panel'
+
+const localStorageKey = '__auth_provider_token__';
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const getToken = () => window.localStorage.getItem(localStorageKey)
+
+export const handleUserReponse = ({user}: {user: User}) => {
+	window.localStorage.setItem(localStorageKey, user.token || " ")
+	return user
+}
+
+export const login = (data: {username: string, password: string}) =>{
+	fetch(`${apiUrl}/register`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	}).then(async (response: Response ) => {
+		if (response.ok) {
+			return handleUserReponse(await response.json())
+		}
+	});
+}
+
+export const register = (data: {username: string, password: string}) =>{
+	fetch(`${apiUrl}/register`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data),
+	}).then(async (response: Response ) => {
+		if (response.ok) {
+			return handleUserReponse(await response.json())
+		}
+	});
+}
+
+
+export const logout =() => window.localStorage.removeItem(localStorageKey)
