c9f752361510bcf7443df259eac2590681fda00f
CAN7 <39960526+CAN1177@users.noreply.github.com>
2021-11-15 22:53:19 +0800

    feat: 新增normalize初始化组件库样式

--- kuiba/src/App.tsx
+++ kuiba/src/App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+// import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1>Hello World!</h1>
+        <h2>Hello World!</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
