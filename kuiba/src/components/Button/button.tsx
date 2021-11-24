a9ec7871879061487f7a4800699b9d61013aa753
CAN7 <39960526+CAN1177@users.noreply.github.com>
2021-11-24 23:51:05 +0800

    feat: 新增storybook

--- kuiba/src/components/Button/button.tsx
+++ kuiba/src/components/Button/button.tsx
@@ -1,43 +1,33 @@
-import React from "react";
+import React, {FC,ButtonHTMLAttributes, AnchorHTMLAttributes} from "react";
 import classNames from "classnames";
-
-// export enum ButtonSize {
-//   Large = "lg",
-//   Small = "sm",
-// }
-
-// export enum ButtonType {
-//   Primary = "primary",
-//   Default = "default",
-//   Danger = "danger",
-//   Link = "link",
-// }
 
 // 枚举改字面量，便于添加
 export type ButtonSize = "lg" | "sm" ;
 export type ButtonType = "primary" | "default" | "danger" | "link";
 
 interface BaseButtonProps {
-  // ?: 为可选参数/属性
   className?: string;
+  /** 是否禁用*/
   disabled?: boolean;
+  /** 尺寸大小*/
   size?: ButtonSize;
   btnType?: ButtonType;
   children?: React.ReactNode;
+  /** link类button*/
   href?: string;
 }
 
 // 类型别名  & 联合类型
 type NativeButtonProps = BaseButtonProps &
-  React.ButtonHTMLAttributes<HTMLElement>;
+  ButtonHTMLAttributes<HTMLElement>;
 type AnchorButtonProps = BaseButtonProps &
-  React.AnchorHTMLAttributes<HTMLElement>;
+  AnchorHTMLAttributes<HTMLElement>;
   // Partial 属性可选
 export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps> ;
 
 
 
-const Button: React.FC<ButtonProps> = (props) => {
+const Button: FC<ButtonProps> = (props) => {
   const { className, btnType, disabled, size, children, href, ...restProps } = props;
 
   // classNames 小工具链接：https://github.com/JedWatson/classnames
