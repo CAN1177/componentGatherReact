1c3b79d23e03c5238f3ad6f1a92f05c642cf3d39
CAN7 <39960526+CAN1177@users.noreply.github.com>
2021-11-29 23:13:39 +0800

    fix: Button storybokk尝试修复

--- kuiba/src/components/Button/button.tsx
+++ kuiba/src/components/Button/button.tsx
@@ -1,51 +1,41 @@
-import React, {FC,ButtonHTMLAttributes, AnchorHTMLAttributes} from "react";
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from "react";
 import classNames from "classnames";
 
-// 枚举改字面量，便于添加
-export type ButtonSize = "lg" | "sm" ;
+export type ButtonSize = "lg" | "sm";
 export type ButtonType = "primary" | "default" | "danger" | "link";
 
 interface BaseButtonProps {
   className?: string;
-  /** 是否禁用*/
+  /**设置 Button 的禁用 */
   disabled?: boolean;
-  /** 尺寸大小*/
+  /**设置 Button 的尺寸 */
   size?: ButtonSize;
+  /**设置 Button 的类型 */
   btnType?: ButtonType;
-  children?: React.ReactNode;
-  /** link类button*/
+  children: React.ReactNode;
   href?: string;
 }
-
-// 类型别名  & 联合类型
-type NativeButtonProps = BaseButtonProps &
-  ButtonHTMLAttributes<HTMLElement>;
-type AnchorButtonProps = BaseButtonProps &
-  AnchorHTMLAttributes<HTMLElement>;
-  // Partial 属性可选
-export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps> ;
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>;
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>;
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
 
 
-
-const Button: FC<ButtonProps> = (props) => {
-  const { className, btnType, disabled, size, children, href, ...restProps } = props;
-
-  // classNames 小工具链接：https://github.com/JedWatson/classnames
+export const Button: FC<ButtonProps> = (props) => {
+  const { btnType, className, disabled, size, children, href, ...restProps } =
+    props;
+  // btn, btn-lg, btn-primary
   const classes = classNames("btn", className, {
     [`btn-${btnType}`]: btnType,
     [`btn-${size}`]: size,
-    disabled: btnType === 'link' && disabled,
+    disabled: btnType === "link" && disabled,
   });
-
-  // 链接形式的button
-  if (btnType === 'link' && href) {
+  if (btnType === "link" && href) {
     return (
       <a className={classes} href={href} {...restProps}>
         {children}
       </a>
     );
   } else {
-    // 普通button
     return (
       <button className={classes} disabled={disabled} {...restProps}>
         {children}
@@ -56,7 +46,7 @@ const Button: FC<ButtonProps> = (props) => {
 
 Button.defaultProps = {
   disabled: false,
-  btnType: 'primary',
+  btnType: "primary",
 };
 
 export default Button;
--- kuiba/src/stories/Button.stories.tsx
+++ kuiba/src/stories/Button.stories.tsx
@@ -1,23 +1,17 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 
-import  Button  from '../components/Button/button';
+import  {Button}  from '../components/Button/button';
 
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
   title: 'Example/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} >Hello world</Button>;
+const Template: ComponentStory<typeof Button> = (args) => <Button {...args} >Hello World</Button>;
 
 export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
+
 Primary.args = {
   disabled: false,
   btnType: 'primary',
