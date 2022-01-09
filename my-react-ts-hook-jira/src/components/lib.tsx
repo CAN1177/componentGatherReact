78316e40a0956ad27ebfa613f399ce645f56c575
CAN7 <39960526+CAN1177@users.noreply.github.com>
2022-01-09 20:51:15 +0800

    feat: 新增全局错误边界

--- my-react-ts-hook-jira/src/components/lib.tsx
+++ my-react-ts-hook-jira/src/components/lib.tsx
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled";
+import { Spin, Typography } from "antd";
+import { DevTools } from "jira-dev-tool";
 
 
 
@@ -22,3 +24,23 @@ export const Row = styled.div<{
         : undefined};
   }
 `;
+
+const FullPage = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const FullPageLoading = () => (
+  <FullPage>
+    <Spin size={"large"} />
+  </FullPage>
+);
+
+export const FullPageErrorFallback = ({ error }: { error: Error | null }) => (
+  <FullPage>
+    <DevTools />
+    <Typography.Text type={"danger"}>{error?.message}</Typography.Text>
+  </FullPage>
+); 
--- my-react-ts-hook-jira/src/context/auth-context.tsx
+++ my-react-ts-hook-jira/src/context/auth-context.tsx
@@ -1,9 +1,11 @@
-import React, { useState, ReactNode } from "react";
+import React, {ReactNode } from "react";
 import * as auth from "auth-provider";
 
 import { User } from "screens/project-list/search-panel";
 import { http } from "utils/http";
 import { useMount } from "utils";
+import { useAsync } from "utils/use-async";
+import { FullPageErrorFallback, FullPageLoading } from "components/lib";
 
 interface AuthForm {
   username: string;
@@ -31,8 +33,9 @@ const AuthContext = React.createContext<{
 AuthContext.displayName = "AuthContext";
  
 export const AuthProvider = ({children}:{children:ReactNode}) => {
-  const [user, setUser] = useState<User | null>(null);
+  // const [user, setUser] = useState<User | null>(null);
   
+  const { data: user, error, isLoading, isIdle, isError, run, setData:setUser } = useAsync<User | null>()
   const login = (form: AuthForm) =>
     // auth.login(form).then((user) => setUser(user));  这就叫做 point free
     auth.login(form).then(setUser); 
@@ -43,9 +46,16 @@ export const AuthProvider = ({children}:{children:ReactNode}) => {
   const logout = () => auth.logout().then(() => setUser(null));
 
   useMount(()=>{
-    bootstrapUser().then(setUser)
+    run( bootstrapUser())
+  //  .then(setUser)
   })
+  if (isIdle || isLoading) {
+    return <FullPageLoading/>
+  }
 
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
 
 	return <AuthContext.Provider children={children} value={{user, login, logout, register}}/>  
 };
